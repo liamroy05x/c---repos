f7f724b99e3ebbef44fbc835087fc59bb7998f7a
liamroy05x <liamroy05x@gmail.com>
2025-12-11 23:59:46 -0500

    Update c++ assignment.cpp
    basic main() + prompt

--- c++ assignment.cpp
+++ c++ assignment.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
 #include <string>
-#include <cctype>
-#include <algorithm>
 
-// parse_input(user_input)
-std::string parse_input(const std::string& user_input) {
-    std::string s = user_input;
+int main() {
+    std::cout << "converter.cpp is running\n";
+    std::cout << "Enter a decimal or hexadecimal number: ";
+
+    std::string user_input;
+    std::getline(std::cin, user_input);
+
+    return 0;
+}

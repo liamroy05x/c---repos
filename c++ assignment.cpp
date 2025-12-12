dbd25dd9b8cf9429c3231c6cc472ab49d25c7523
liamroy05x <liamroy05x@gmail.com>
2025-12-11 23:54:41 -0500

    Create c++ assignment.cpp
    created file

---
+++ c++ assignment.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <string>
+#include <cctype>
+#include <algorithm>
+
+// parse_input(user_input)
+std::string parse_input(const std::string& user_input) {
+    std::string s = user_input;

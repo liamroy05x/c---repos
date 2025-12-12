8f199c0664afc6e8580a92ef72777cf2b8c3beac
liamroy05x <liamroy05x@gmail.com>
2025-12-12 00:01:17 -0500

    Update c++ assignment.cpp
    trim whitespace + empty check in parse_input

--- c++ assignment.cpp
+++ c++ assignment.cpp
@@ -1,10 +1,19 @@
 #include <iostream>
 #include <string>
 
-#include <iostream>
-#include <string>
-
 std::string parse_input(const std::string& user_input) {
+    std::string s = user_input;
+
+    // trim whitespace
+    s.erase(0, s.find_first_not_of(" \t\n\r"));
+    if (!s.empty()) {
+        s.erase(s.find_last_not_of(" \t\n\r") + 1);
+    }
+
+    if (s.empty()) {
+        return "unknown";
+    }
+
     return "unknown";
 }
 
